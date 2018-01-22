0a537c2d53bcb7164ec18cf2fc98d84d3978a66c
Randolph <wolftotem4@gmail.com>
2018-01-22 23:37:29 +0800

    adjust composer.json and rewrite 'redirectUri' implement

--- src/OWolf/Facebook/FacebookCredentialsProvider.php
+++ src/OWolf/Facebook/FacebookCredentialsProvider.php
@@ -6,6 +6,7 @@ use Illuminate\Support\ServiceProvider;
 use League\OAuth2\Client\Token\AccessToken;
 use OWolf\Credentials\UrlAccessToeknCredentials;
 use OWolf\Laravel\UserOAuthManager;
+use OWolf\Laravel\Util;
 
 class FacebookCredentialsProvider extends ServiceProvider
 {
@@ -15,9 +16,7 @@ class FacebookCredentialsProvider extends ServiceProvider
             $manager->addDriver('facebook.oauth', function ($name, $config) {
                 $oauth = array_get($config, 'oauth', []);
 
-                $oauth['redirectUri'] = isset($oauth['redirectUri'])
-                    ? value($oauth['redirectUri'])
-                    : route('oauth.callback', [$name]);
+                $oauth['redirectUri'] = Util::redirectUri(array_get($oauth, 'redirectUri'), $name);
 
                 $provider = new Facebook($oauth);
                 return new FacebookOAuthHandler($provider, $name, $config);
